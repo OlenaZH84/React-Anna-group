57845302cccde2240e867ad4436db48c4c5006d0
OlenaZH84 <elenazhovtiuk@gmail.com>
2023-03-17 14:20:35 +0200

    Added Error component(catch the error)

--- week2/project/ecommerce2/src/components/ProductDetails.js
+++ week2/project/ecommerce2/src/components/ProductDetails.js
@@ -1,40 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import ErrorMsg from './Error';
 
 export default function ProductDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const getProduct = async () => {
+  const [errorObj, setErrorObj] = useState({ isError: false, msg: '' });
+  const getProduct = async () => {
+    try {
       setLoading(true);
       const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+      if (!response.ok) {
+        throw new Error(`Error happend ${response.status} ${response.statusText}`);
+      }
       const data = await response.json();
       setProduct(data);
-
+    } catch (error) {
+      setErrorObj({ isError: true, msg: error.message });
+    } finally {
       setLoading(false);
-    };
-    getProduct();
-  }, [id]);
+    }
+  };
+  useEffect(() => {
+    getProduct(id);
+  }, []);
+  // useEffect(() => {
+  //   const getProduct = async () => {
+  //     setLoading(true);
+  //     const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+  //     const data = await response.json();
+  //     setProduct(data);
+
+  //     setLoading(false);
+  //   };
+  //   getProduct();
+  // }, [id]);
 
   if (loading) {
     return <h3>Loading...</h3>;
   }
 
   return (
-    <div className="product-details">
-      <div className="title-container">
-        <h1 className="title-container--title">{product.title}</h1>
-      </div>
-      <div className="product-details--information">
-        <div className="product-details--image">
-          <div className="product-image--container">
-            <img className="product-image" src={product.image} alt={product.title} />
-          </div>
+    <>
+      {errorObj.isError && <ErrorMsg msg={errorObj.msg} />}
+      <div className="product-details">
+        <div className="title-container">
+          <h1 className="title-container--title">{product.title}</h1>
         </div>
-        <p className="product-details--description">{product.description}</p>
+        <div className="product-details--information">
+          <div className="product-details--image">
+            <div className="product-image--container">
+              <img className="product-image" src={product.image} alt={product.title} />
+            </div>
+          </div>
+          <p className="product-details--description">{product.description}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
